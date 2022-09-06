ad9a5bb618674adba60055afef8ff7a826f259e3
Ramya Non-IU <ramyaram2092@outlook.com>
2022-09-06 14:20:22 -0400

    Reading from file works. Used while loop to read all lines

--- smtp/main.c
+++ smtp/main.c
@@ -64,7 +64,7 @@ int main(int argc, char* argv[]) {
    printf("\n RCPT TO response is %s",response);
    send_smtp(socket,"DATA \n",response,4096);
    printf("\n DATA response is %s",response);
-   printf("\n message read from the file is %s",buff);
+   printf("\n message read from the file is %s",message);
    send_smtp(socket,message,response,4096);
    fclose(fp);
    printf("\n final response is %s",response);
