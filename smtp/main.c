7425300f12201e1b143827d526c819961492ab6c
Ramya Non-IU <ramyaram2092@outlook.com>
2022-09-06 13:20:33 -0400

    debugging data command and message

--- smtp/main.c
+++ smtp/main.c
@@ -50,10 +50,10 @@ int main(int argc, char* argv[]) {
    printf(" MAIL FROM Response is %s",response);
    send_smtp(socket,recptTo_cmd,response,4096);
    printf("RCPT TO response is %s",response);
-   send_smtp(socket,"DATA",response,4096);
-   printf("DATA response is %s",response);
-  //  send_smtp(socket,"This is test data.\r\n.\r\n ",response,4096);
-  //  printf("final response is %s",response);
+  //  send_smtp(socket,"DATA",response,4096);
+  //  printf("DATA response is %s",response);
+   send_smtp(socket,"This is test data.\r\n.\r\n ",response,4096);
+   printf("final response is %s",response);
 
 
 
