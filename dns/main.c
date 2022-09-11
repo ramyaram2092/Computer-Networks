7f0515d21239a302417d1486a695120253d10d42
Ramya Non-IU <ramyaram2092@outlook.com>
2022-09-10 22:22:15 -0400

    Added addrinfo struct and getaddrinfo function

--- dns/main.c
+++ dns/main.c
@@ -3,7 +3,20 @@
 #include <sys/types.h>
 #include<sys/socket.h>
 #include<netdb.h>
-
+int getaddrinfo(const char *restrict node,
+                const char *restrict service,
+                const struct addrinfo *restrict hints,
+                struct addrinfo **restrict res);
+struct addrinfo {
+    int         ai_flags;
+    int         ai_family;
+    int         ai_socktype;
+    int         ai_protocol;
+    socklen_t   ai_addrlen;
+    struct sockaddr *ai_addr;
+    char *      ai_canonname;
+    struct addrinfo *ai_next;
+  };
 /*
   Use the `getaddrinfo` and `inet_ntop` functions to convert a string host and
   integer port into a string dotted ip address and port.
@@ -19,10 +32,18 @@ int main(int argc, char* argv[]) {
   snprintf(buff,128,"%ld",process);
   char* port=buff;
  
-
-  printf("\n Host : %s",host);
-  printf("\n Port: %s",port);
+   
+  // printf("\n Host : %s",host);
+  // printf("\n Port: %s",port);
+  struct addrinfo *hints;
+  hints.ai_flags=AI_PASSIVE;
+  hints.ai_family=PF_UNSPEC;
+  hints.ai_socktype=SOCK_STREAM;
+  hints.ai_protocol=IPPROTO_TCP;
+  struct addrinfo **response;
+    
   
-
+  getaddrinfo(host,port,addrinfo hints,response);
+  printf("I sucessfully contacted the server");
   return 0;
 }
