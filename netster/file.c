ae7f6530902a3617bec1791085de2fd4f0079aec
Ramya Non-IU <ramyaram2092@outlook.com>
2022-10-24 16:40:33 -0400

    txt file transfer in TCP works successfully

--- netster/file.c
+++ netster/file.c
@@ -1,9 +1,20 @@
 #include "file.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+
 void tcp_server_ft(char *iface, long port, FILE *fp);
 void tcp_client_ft(char *host, long port, FILE *fp);
 void udp_server_ft(char *iface, long port, FILE *fp);
 void udp_client_ft(char *host, long port, FILE *fp);
+
+const char *inet_ntop(int af, const void *restrict src,
+                      char *restrict dst, socklen_t size);
 
 void file_server(char *iface, long port, int use_udp, FILE *fp)
 {
@@ -28,7 +39,13 @@ void file_client(char *host, long port, int use_udp, FILE *fp)
         udp_client_ft(host, port, fp);
     }
 }
-
+/**
+ * @brief TCP server
+ *
+ * @param iface
+ * @param port
+ * @param fp
+ */
 void tcp_server_ft(char *iface, long port, FILE *fp)
 {
     int serverSocket, newSocket;
@@ -77,11 +94,118 @@ void tcp_server_ft(char *iface, long port, FILE *fp)
         exit(0);
     }
 
-    int i = 0;
+    // listen to the socket connection
+    if (listen(serverSocket, 3) != 0) // the no 5 is subjected to change. no of requests that can be queued
+    {
+        printf("TCP : Listening failed\n ");
+        exit(0);
+    }
+    socklen_t clientLen = sizeof(client);
+
+    // accept the incoming packet from client
+
+    newSocket = accept(serverSocket, (struct sockaddr *)&client, &clientLen);
+
+    if (newSocket < 0)
+    {
+        printf("TCP: Unable to accept the client packet\n ");
+        exit(0);
+    }
+
+    char message[256];
+    if ((recv(newSocket, message, sizeof(message), 0) < 0))
+    {
+            printf("TCP: Coundnt recieve file from client\n");
+            exit(0);
+    }
+     fprintf(fp, "%s", message);
+    // recieve message if any
+    while (strlen(message)!=0)
+    {
+        printf("\n reading file");
+        bzero(message, sizeof(message));
+        if ((recv(newSocket, message, sizeof(message), 0) < 0))
+        {
+            printf("TCP: Coundnt recieve file from client\n");
+            exit(0);
+        }
+        fprintf(fp, "%s", message);
+        
+       
+    }
+
+    printf("the file was received successfully");
+    close(newSocket);
+    close(serverSocket);
 }
+/**
+ * @brief TCP client
+ *
+ * @param host
+ * @param port
+ * @param fp
+ */
 
 void tcp_client_ft(char *host, long port, FILE *fp)
 {
+    int clientSocket;
+    struct sockaddr_in serveraddr;
+
+    // create socket
+    clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (clientSocket < 0)
+    {
+        printf("TCP: Client socket creation failed \n");
+        exit(1);
+    }
+
+    // resolve address
+
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+
+    hints.ai_flags = AI_PASSIVE;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = 0;
+
+    struct addrinfo *response;
+    response = (struct addrinfo *)malloc(sizeof(struct addrinfo));
+    char str[256];
+    sprintf(str, "%ld", port);
+    getaddrinfo(host, str, &hints, &response);
+    struct addrinfo *iterator = response;
+    char buffer[4096];
+    void *raw_addr;
+
+    struct sockaddr_in *tmp = (struct sockaddr_in *)iterator->ai_addr;
+    raw_addr = &(tmp->sin_addr);
+    inet_ntop(AF_INET, raw_addr, buffer, 4096);
+
+    // assign ip and port
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = inet_addr(buffer);
+    serveraddr.sin_port = htons(port);
+
+    // connect client socket with  server socket
+    if (connect(clientSocket, (struct sockaddr *)&serveraddr, sizeof(serveraddr)) != 0)
+    {
+        printf("TCP: Connection with server : %s  and port %s failed\n", buffer, str);
+        exit(1);
+    }
+    char message[256]={0};
+
+    while (fgets(message,256,fp) != NULL)
+    {
+        // send the file
+        if (send(clientSocket, message, strlen(message), 0) < 0)
+        {
+            printf("TCP: Sending file from client failed\n");
+            exit(1);
+        }
+    }
+    printf("the file was sent successfully");
+    close(clientSocket);
 }
 
 void udp_server_ft(char *iface, long port, FILE *fp)
